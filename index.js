b27110bdc9ee33752499fcfc2a44f9bf2100833d
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-25 02:42:07 -0700

    added prompts

---
+++ index.js
@@ -0,0 +1,122 @@
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const inquirer = require("inquirer");
+const { exit } = require("process");
+//const generateHTML = require("./dist/generateHTML")
+
+var employees = [];
+
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                message: 'Enter the manager\'s name: ',
+                name: 'name',
+            },
+            {
+                message: 'What is their ID number?',
+                name: 'id'
+            },
+            {
+                message: 'What is their email address?',
+                name: 'email'
+            },
+            {
+                message: 'What is their office number?',
+                name: 'office'
+            },
+        ])
+        .then(({ name, id, email, office }) => {
+            const manager = new Manager(name, id, email, office);
+            employees.push(manager);
+            selectEmployee();
+        })
+        .catch((err) => {
+            console.error(err.message)
+        })
+}
+
+function selectEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do next?",
+                choices: ['Add an engineer', 'Add an intern', 'Finished building team'],
+                name: choice
+            },
+        ])
+        .then((response) => {
+            switch (response.choice) {
+                case 'Add an engineer':
+                    addEngineer();
+                    break;
+                case 'Add an intern':
+                    addIntern();
+                    break;
+                case 'Finished building team':
+                    exit()
+            }
+        })
+}
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                message: 'Enter the engineer\'s name?',
+                name: 'name',
+            },
+            {
+                message: 'What is their ID number?',
+                name: 'id'
+            },
+            {
+                message: 'What is their email address?',
+                name: 'email'
+            },
+            {
+                message: 'What is their Github id?',
+                name: 'github'
+            },
+        ])
+        .then(({ name, id, email, github }) => {
+            const engineer = new Manager(name, id, email, github);
+            employees.push(engineer);
+            selectEmployee();
+        })
+        .catch((err) => {
+            console.error(err.message)
+        })
+}
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                message: 'Enter the intern\'s name?',
+                name: 'name',
+            },
+            {
+                message: 'What is their ID number?',
+                name: 'id'
+            },
+            {
+                message: 'What is their email address?',
+                name: 'email'
+            },
+            {
+                message: 'What school are they currently attending?',
+                name: 'school'
+            },
+        ])
+        .then(({ name, id, email, school }) => {
+            const school = new Manager(name, id, email, school);
+            employees.push(school);
+            selectEmployee();
+        })
+        .catch((err) => {
+            console.error(err.message)
+        })
+}
