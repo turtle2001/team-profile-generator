017d36628763643b6d5ae5c0b2bd343015c08c22
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-25 23:48:03 -0700

    created html file

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import Manager from './lib/Manager.js'
 import Engineer from './lib/Engineer.js'
 import Intern from './lib/Intern.js'
 import inquirer from 'inquirer'
-const generateHTML = require("./dist/generateHTML")
-const fs = require("fs")
+import fs from 'fs'
 
 let employees = [];
 
@@ -116,25 +115,61 @@ function addIntern() {
 }
 
 function generateHTML(employees) {
+    let cards = employees.map(employee => generateCard(employee))
     let generatedHtml =
         `<!DOCTYPE html>
     <html lang="en">
+    
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <title>My Team</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>My Team</title>
     </head>
-    <header class="jumbotron text-center text-white">
-    <h1>My Team</h1>
-    </header>
-    <body class="container">
+    
+    <body>
+        <div class="jumbotron text-center bg-danger text-white">
+            <h1>My Team</h1>
+        </div>
+        <div class="container">
             <div class="row justify-content-around">
-            ${cards.join("\n")}
+                ${cards.join("\n")}
             </div>
-    </body>
-    </html>`
+        </div>
+    </body>        
+</html>
+`
+    fs.writeFile(`./dist/generatedhtml.html`, generatedHtml, (err) => {
+        if (err)
+            console.log(err)
+        else
+            console.log("File created")
+    })
+}
+
+function generateCard(employee) {
+    return `
+<div class="col-4">
+    <div class="card m-2 shadow">
+        <div class="card-header bg-primary text-white">
+            <h4>${employee.getName()}</h4>
+            <h5><i class="fa-solid fa-mug-hot"></i>${employee.getRole()}</h5>
+        </div>
+        <div class="card-body bg-light">
+            <div class="list-group">
+                <li class="list-group-item">ID: ${employee.getId()}</li>
+                <li class="list-group-item">Email: <a
+                        href="email: ${employee.getEmail()}">${employee.getEmail()}</a> </li>
+                <li class="list-group-item">${employee.getExtraName()}: ${employee.getExtraInfo()}</li>
+            </div>
+        </div>
+    </div>
+</div>`
 }
 
 addManager();
