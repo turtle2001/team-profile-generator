9df79911e5df2fbc8deddc80d321fc65c0bf2afe
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-25 23:11:40 -0700

    html template

--- index.js
+++ index.js
@@ -126,15 +126,13 @@ function generateHTML(employees) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <title>My Team</title>
     </head>
-    <body>
-        <div class="jumbotron text-center bg-danger text-white">
-            <h1>My Team</h1>
-        </div>
-        <div class="container">
+    <header class="jumbotron text-center text-white">
+    <h1>My Team</h1>
+    </header>
+    <body class="container">
             <div class="row justify-content-around">
             ${cards.join("\n")}
             </div>
-        </div>
     </body>
     </html>`
 }
